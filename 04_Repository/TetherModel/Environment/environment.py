82ed97d5df7402437b43ca95d557037b9779201b
Tom Woodley <thwoodley@gmail.com>
2024-01-29 22:28:41 +0000

    refactored environment class

--- 04_Repository/TetherModel/Environment/environment.py
+++ 04_Repository/TetherModel/Environment/environment.py
@@ -5,8 +5,4 @@ class Environment:
     def __init__(self):
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.ground = p.loadURDF("plane.urdf")
-        # Load other static or dynamic objects into the environment
-        
-    def add_obstacle(self, obstacle):
-        # Add an obstacle to the environment
-        pass
+        # TODO: Add the perching branch
