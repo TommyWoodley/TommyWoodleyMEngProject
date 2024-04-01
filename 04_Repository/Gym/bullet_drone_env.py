a472b2c02643015cd18179855560d3a28c954ea8
Tom Woodley <thwoodley@gmail.com>
2024-04-01 19:30:46 +0100

    refactored to seperate generating method

--- 04_Repository/Gym/bullet_drone_env.py
+++ 04_Repository/Gym/bullet_drone_env.py
@@ -21,7 +21,7 @@ class BulletDroneEnv(gym.Env):
 
     def __init__(self, render_mode: str = "human") -> None:
         super(BulletDroneEnv, self).__init__()
-        self.simulator = TetheredDroneSimulator(drone_pos=np.array(self.reset_pos, dtype=np.float32), gui_mode=(render_mode == "human"))
+        self.simulator = TetheredDroneSimulator(drone_pos=self._generate_reset_position(42), gui_mode=(render_mode == "human"))
         self.action_space = spaces.Box(low=np.array([-0.001, -0.001, -0.001]),
                                        high=np.array([0.001, 0.001, 0.001]), dtype=np.float32)
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32)
@@ -31,7 +31,7 @@ class BulletDroneEnv(gym.Env):
 
     def reset(self, seed: int = None, options: Dict[str, Any] = None) -> Tuple[np.ndarray, Dict[Any, Any]]:
         super().reset(seed=seed, options=options)
-        self.simulator.reset(self._generate_reset_position)
+        self.simulator.reset(self._generate_reset_position(seed))
         self.num_steps = 0
         return self.reset_pos, {}
 
@@ -64,3 +64,6 @@ class BulletDroneEnv(gym.Env):
         # Implement how reward is calculated based on the state
         distance = np.linalg.norm(state - self.goal_state)
         return - distance, bool(distance < 0.1), False
+    
+    def _generate_reset_position(self, seed):
+        return np.array(self.reset_pos, dtype=np.float32)
