63587fc643eaae3fd11dc4452ea9d3e907053590
Tom Woodley <thwoodley@gmail.com>
2024-04-15 15:31:18 +0100

    add basic loading

--- 04_Repository/main.py
+++ 04_Repository/main.py
@@ -1,4 +1,4 @@
-
+from Gym.Algorithms.sacfd import SACfD
 from Gym.bullet_drone_env import BulletDroneEnv
 from Gym.Wrappers.two_dim_wrapper import TwoDimWrapper
 from Gym.Wrappers.position_wrapper import PositionWrapper
@@ -7,28 +7,52 @@ from stable_baselines3.common.monitor import Monitor
 import argparse
 import datetime
 import os
+import numpy as np
+import json
+import glob
 
 
 def main(algorithm, num_steps, filename):
-    print(f"Algorithm: {algorithm}")
-    print(f"Number of Steps: {num_steps}")
+    print_green(f"Algorithm: {algorithm}")
+    print_green(f"Number of Steps: {num_steps}")
     save_data = filename is not None
     if save_data:
         dir_name = get_dir_name(filename)
-    print(f"File Name: {dir_name}")
+        os.mkdir(f"models/{dir_name}")
+        print_green(f"File Name: {dir_name}")
+    else:
+        print_red("WARNING: No output or logs will be generated, the model will not be saved!")
 
-    os.mkdir(f"models/{dir_name}")
     env = PositionWrapper(TwoDimWrapper(BulletDroneEnv(render_mode="console")))
     if save_data:
         env = Monitor(env, f"models/{dir_name}/logs")
     if algorithm == "SAC":
         model = train_sac(env, num_steps)
+    elif algorithm == "SACfD":
+        model = train_sacfd(env, num_steps)
     else:
-        print("ERROR: Not yet implemented")
+        print_red("ERROR: Not yet implemented")
+    print_green("TRAINING COMPLETE!")
     if save_data:
         model.save(f"models/{dir_name}/model")
+        print_green("Model Saved")
     env.close()
 
+    {
+        "state": [
+            2.2702350087543315,
+            3.058575988926993
+        ],
+        "action": [
+            -0.24440059558633465,
+            0.05260709407333408
+        ],
+        "reward": -2.270990563928559,
+        "next_state": [
+            2.025834413167997,
+            3.111183083000327
+        ]
+    },
 
 def train_sac(env, num_steps):
     model = SAC(
@@ -42,6 +66,39 @@ def train_sac(env, num_steps):
 
     return model
 
+def train_sacfd(env, num_steps):
+    model = SAC(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        seed=0,
+        batch_size=32,
+        policy_kwargs=dict(net_arch=[64, 64]),
+    )
+
+    data = get_buffer_data()
+    print("Buffer Size: ", model.replay_buffer.size())
+
+    for obs, next_obs, action, reward, done, info in data:
+        model.replay_buffer.add(obs, next_obs, action, reward, done, info)
+    print("Buffer Size: ", model.replay_buffer.size())
+    model.learn(num_steps, log_interval=10, progress_bar=True)
+
+    return model
+
+def get_buffer_data():
+    dir = "Data/PreviousWorkTrajectories/rl_demos"
+    return load_all_data(dir)
+
+def load_all_data(directory):
+    pattern = f"{directory}/rl_demo_approaching_angle_*.json"
+    files = glob.glob(pattern)
+    all_data = []
+    for file in files:
+        json_data = load_json(file)
+        transformed_data = convert_data(json_data)
+        all_data.extend(transformed_data)
+    return all_data
 
 def get_dir_name(prefix):
     # Get the current date and time
@@ -51,6 +108,29 @@ def get_dir_name(prefix):
 
     return dir_name
 
+# Load the JSON data from a file
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def convert_data(json_data):
+    dataset = []
+    for item in json_data:
+        obs = np.array(item['state'])
+        next_obs = np.array(item['next_state'])
+        action = np.array(item['action'])
+        reward = np.array(item['reward'])
+        done = np.array([False])
+        info = [{}]
+        dataset.append((obs, next_obs, action, reward, done, info))
+    return dataset
+
+def print_red(text):
+    print(f"\033[31m{text}\033[0m")
+
+def print_green(text):
+    print(f"\033[32m{text}\033[0m")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process input parameters.")
@@ -59,7 +139,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--num_steps", type=int, required=True,
                         help="Specify the number of steps e.g., 4000")
     parser.add_argument("-f", "--filename", type=str,
-                        default=f"simple_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}.py",
+                        default=None,
                         help="Optional: Specify the file name. Defaults to 'simple_YYYYMMDD_HHMM.py'")
 
     args = parser.parse_args()
