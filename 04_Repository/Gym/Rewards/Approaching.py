609ee7118089438904a97133de28c5da83d4b9f4
Tom Woodley <thwoodley@gmail.com>
2024-04-15 13:46:10 +0100

    style fixes

--- 04_Repository/Gym/Rewards/Approaching.py
+++ 04_Repository/Gym/Rewards/Approaching.py
@@ -2,8 +2,10 @@ import math
 from typing import Tuple
 import numpy as np
 
+
 def interpolate_distance(distance, max_value, max_reward, min_value=0, min_reward=0):
     return min_reward + ((max_reward - min_reward) * (distance - min_value)) / (max_value - min_value)
+
 
 class CircularApproachingReward():
     down_centre_x, down_centre_y, down_start, down_end = 0, 2.7, 225, 315
@@ -19,12 +21,12 @@ class CircularApproachingReward():
     def _calculate_sector_reward(self, state):
         x, _, z = state
         is_within, norm_distance = self._within_sector(self.down_centre_x, self.down_centre_y, self.radius,
-                                                      self.down_start, self.down_end, x, z)
+                                                       self.down_start, self.down_end, x, z)
         if is_within:
             return - self.reward_penalty * norm_distance
 
         is_within, norm_distance = self._within_sector(self.up_centre_x, self.up_centre_y, self.radius,
-                                                      self.up_start, self.up_end, x, z)
+                                                       self.up_start, self.up_end, x, z)
         if is_within:
             return - self.reward_penalty * norm_distance
 
@@ -32,21 +34,22 @@ class CircularApproachingReward():
 
     def _within_sector(self, center_x, center_y, radius, start_angle, end_angle, point_x, point_y):
         distance = math.sqrt((point_x - center_x) ** 2 + (point_y - center_y) ** 2)
-        
+
         # Check if the point is within the circle's radius
         if distance > radius:
             return False, None
-        
+
         # Calculate the angle from the center to the point in radians
         angle_radians = math.atan2(point_y - center_y, point_x - center_x)
-        
+
         # Convert angle to degrees for easier handling, normalizing to [0, 360)
         angle_degrees = math.degrees(angle_radians) % 360
         start_angle %= 360
         end_angle %= 360
-        
+
         if start_angle > end_angle:
-            within = (angle_degrees >= start_angle and angle_degrees <= 360) or (angle_degrees >= 0 and angle_degrees <= end_angle)
+            within = (angle_degrees >= start_angle and angle_degrees <= 360) or (
+                angle_degrees >= 0 and angle_degrees <= end_angle)
         else:
             within = (start_angle <= angle_degrees <= end_angle)
         
@@ -63,7 +66,7 @@ class CircularApproachingReward():
         """
         if dist_drone_branch < 0.1:  # A collision
             return -5.0
-        elif dist_drone_branch < 0.2: # Quite close
+        elif dist_drone_branch < 0.2:  # Quite close
             return interpolate_distance(dist_drone_branch, 0.1, -5, min_value=0.2)
         else:
             return 0
