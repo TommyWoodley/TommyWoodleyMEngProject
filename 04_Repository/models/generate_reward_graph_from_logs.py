22c6133888fdc14b49536df4b70d12e021d7efba
Tom Woodley <thwoodley@gmail.com>
2024-04-23 13:08:43 +0100

    allow custom num of episodes in plotting

--- 04_Repository/models/generate_reward_graph_from_logs.py
+++ 04_Repository/models/generate_reward_graph_from_logs.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.graphics.plot_rl_rewards_training import plot_rl_reward_graph
 
 
-def read_csv_file(filename, show=True):
+def read_csv_file(filename, num_episodes=None, show=True):
     """
     Reads a CSV file and prints its contents.
 
@@ -19,8 +19,11 @@ def read_csv_file(filename, show=True):
         directory = os.path.dirname(filename)
         output_filename = os.path.join(directory, 'rewards_graph.png')
 
-        rewards = data['r'].iloc[0:300]
-        lengths = data['l'].iloc[0:300]
+        rewards = data['r']
+        lengths = data['l']
+        if num_episodes is not None:
+            rewards = rewards.iloc[0:num_episodes]
+            lengths = lengths.iloc[0:num_episodes]
         plot_rl_reward_graph(rewards, episode_lens=lengths, output_filename=output_filename, show_plot=show)
     except FileNotFoundError:
         print("Error: File not found. Please check the filename and try again.")
@@ -34,8 +37,13 @@ def read_csv_file(filename, show=True):
 
 if __name__ == "__main__":
     # Check if the filename is given as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python generate_reward_graph_from_logs.py <filename>")
-    else:
+    if len(sys.argv) == 2:
         filename = sys.argv[1]
         read_csv_file(filename, show=True)
+    elif len(sys.argv) == 3:
+        filename = sys.argv[1]
+        num_episodes = int(sys.argv[2])
+        read_csv_file(filename, num_episodes=num_episodes, show=True)
+    else:
+        print("Usage: python generate_reward_graph_from_logs.py <filename> <num>")
+        
