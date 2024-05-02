dc1b6def39f1171e458403926d953f7fb29b5742
Tom Woodley <thwoodley@gmail.com>
2024-05-02 10:00:01 +0100

    revert run tethered drone simulator

--- 04_Repository/TetherModel/Environment/run_tethered_drone_simulator.py
+++ 04_Repository/TetherModel/Environment/run_tethered_drone_simulator.py
@@ -1,11 +1,6 @@
 from tethered_drone_simulator import TetheredDroneSimulator
 from typing import List
 import numpy as np
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from Gym.Rewards.Approaching import CircularApproachingReward
-import matplotlib.pyplot as plt
 
 
 class TetheredDroneSimulatorRunner:
@@ -15,13 +10,8 @@ class TetheredDroneSimulatorRunner:
         self.xs = xs
         self.zs = zs
         self.iteration = 0
-        self.circular_reward = CircularApproachingReward()
-        self.rewards = []
 
     def run(self) -> None:
-        plt.ion()  # Turn on the interactive mode in matplotlib
-        fig, ax = plt.subplots()
-
         already_moved = False
         action_size = None
         action_mags = []
@@ -35,25 +25,12 @@ class TetheredDroneSimulatorRunner:
             action = drone_pos - self.prev_pos
             action_mags.append(np.linalg.norm(action))
             if self.iteration < len(self.xs) * 2:
-                has_collided, dist_tether_branch, dist_drone_branch, full = self.simulator.step(action)
+                self.simulator.step(action)
             elif not already_moved:
-                has_collided, dist_tether_branch, dist_drone_branch, full = self.simulator.step(np.array([-0.2, 0, 0], dtype=np.float32))
+                self.simulator.step(np.array([-0.2, 0, 0], dtype=np.float32))
                 already_moved = True
                 action_size = np.mean(action_mags)
             else:
-                has_collided, dist_tether_branch, dist_drone_branch, full = self.simulator.step()
+                self.simulator.step()
             self.prev_pos = drone_pos
-            state = self.simulator.drone_pos
-            reward, _, _ = self.circular_reward.reward_fun(state, has_collided, dist_tether_branch, dist_drone_branch, full)
-            print("x: ", x, " z: ", z, "action_mag: ", action_size, "reward: ", reward)
-
-            # Append reward to the list
-            self.rewards.append(reward)
-
-            # Plot or update the graph
-            ax.clear()
-            ax.plot(self.rewards)
-            ax.set_title('Reward Over Time')
-            ax.set_xlabel('Iterations')
-            ax.set_ylabel('Reward')
-            # plt.pause(0.05)  # Pause a bit for the plot to update
+            print("x: ", x, " z: ", z, "action_mag: ", action_size)
