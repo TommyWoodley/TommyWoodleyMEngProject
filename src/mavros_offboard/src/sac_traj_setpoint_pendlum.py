f7a546839b7cb214e1b1f0dfa7f29e4b45e31ddc
Tom Woodley <thwoodley@gmail.com>
2024-05-29 23:46:48 +0100

    fix syntax error

--- src/mavros_offboard/src/sac_traj_setpoint_pendlum.py
+++ src/mavros_offboard/src/sac_traj_setpoint_pendlum.py
@@ -526,7 +526,7 @@ class MavrosOffboardSuctionMission():
         pose.pose.position.z = self.pos.pose.position.z
 
         # sent point until time is over
-        self.ros_log_info("About to begin waiting for " + time + "seconds.")
+        self.ros_log_info("About to begin waiting for " + str(time) + "seconds.")
         for i in range(num_waiting_steps):
             if rospy.is_shutdown():
                 break
